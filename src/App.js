153c87c014f8bbc41ef15912cbe3359974c28d01
KyryloSafonov <kirilsafon@gmail.com>
2020-12-25 18:28:45 +0200

    React_HW_6_part1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
-import Users from "./Users";
+
+
+
 
 class App extends Component {
+
+     inputMain = React.createRef();
+
+     state = {inputValue: ''}
+
+    oninputFill = ()=> {
+         console.log(this.inputMain.current.value);
+        this.setState({inputValue: this.inputMain.current.value});
+    }
+    onFormSubmit = (e)=>{
+         e.preventDefault();
+    };
+
     render() {
+        let {inputValue} = this.state;
         return (
             <div>
-                <Users/>
+                <form onSubmit={this.onFormSubmit}>
+                    <input ref={this.inputMain} type={'numbers'} onInput={this.oninputFill} value={inputValue}/>
+                    <button>send</button>
+
+                </form>
+
             </div>
         );
     }
